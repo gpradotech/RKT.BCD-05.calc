571a07307c606124d1ee55283d604c3ecf9e3564
contato.gprado@gmail.com <contato.gprado@gmail.com>
2025-05-05 18:44:45 -0300

    adiciona script para manipulação da calculadora e corrige id do resultado

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// display
+const resultNumber = document.getElementById("resultNumber");
+const calc = document.getElementById("calc");
+
+// numbers
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const decimal = document.getElementById("decimal");
+
+// operators
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+const subtract = document.getElementById("subtract");
+const add = document.getElementById("add");
+const equals = document.getElementById("equals");
+
+// other buttons
+const backspace = document.getElementById("backspace");
+const percent = document.getElementById("percent");
+const clear = document.getElementById("clear");
+
+//////////////////////////////////////////////////////////////////
+
